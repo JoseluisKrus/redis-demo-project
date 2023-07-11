2cbce43f131978ba62423713651ad65fd81f711c
Hoseluis Angelov <Hoseluiskrus@gmail.com>
2023-07-11 11:36:42 +0300

    Connect to Redis

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const redis = require("redis");
+
+const redisURL = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisURL);
+
 
 const app = express();
+app.use(express.json())
 
 app.listen(8080, () =>{
     console.log("Hey, now listening on port 8080!")
